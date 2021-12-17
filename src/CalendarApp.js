bdbb5170ac5ef8e494f8d4356aa4370f66144ee3
Catalina Carreto <iamkcoder@gmail.com>
2021-12-17 11:53:39 -0600

    feat: Create LoginScreen and CalendarScreen components

--- src/CalendarApp.js
+++ src/CalendarApp.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { AppRouter } from './router/AppRouter';
 
 function CalendarApp() {
   return (
     <div>
-      <h1>Hola Mundo</h1>
+      <AppRouter />
     </div>
   );
 }
