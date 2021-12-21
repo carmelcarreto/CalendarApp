d1535b2e74bed379e0a73c9ee24a4f9453bb27fd
Catalina Carreto <iamkcoder@gmail.com>
2021-12-21 15:14:28 -0600

    feat: Add new event in CalendarModal component

--- src/components/calendar/CalendarModal.js
+++ src/components/calendar/CalendarModal.js
@@ -6,6 +6,7 @@ import moment from 'moment';
 import Swal from 'sweetalert2';
 
 import { uiCloseModal } from '../../actions/ui';
+import { eventAddNew } from '../../actions/events';
 
 
 const customStyles = {
@@ -84,6 +85,15 @@ export const CalendarModal = () => {
         if(title.trim().length < 2){
             return setTitleValid(false);
         }
+        dispatch(eventAddNew({
+            ...formValues,
+            id: new Date().getTime(),
+            user: {
+                _id: '12345',
+                name: 'Katye'
+            }
+        }));
+
         setTitleValid(true);
         closeModal();
     }
