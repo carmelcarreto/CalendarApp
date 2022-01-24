a5e1d00ce2608f9e99fb59c11412a829486f864e
Catalina Carreto <iamkcoder@gmail.com>
2022-01-24 12:39:56 -0600

    test: Create test in authReducer

---
+++ src/tests/reducers/authReducer.test.js
@@ -0,0 +1,34 @@
+import { authReducer } from '../../reducers/authReducer';
+import { types } from '../../types/types';
+
+const initState = {
+    checking: true,
+    //uid: null,
+    //name: null
+}
+
+describe(`Pruebas en authReducer.js`, () => {
+    
+    test(`debe de retornar el estado por defecto`, () => {  
+        const action = {};
+
+        const state = authReducer( initState, action);
+        expect( state ).toEqual( initState );
+    
+    });
+
+    test(`debe de autenticar el usuario`, () => {
+        const action = {
+            type: types.authLogin,
+            payload: {
+                id: '123',
+                name:'Katy'
+            }
+        };
+        const state = authReducer(initState, action);
+        expect(state).toEqual({ checking: false, id: '123', name: 'Katy' });
+    
+    });
+
+    
+});
