7fcdee3d9bade89a112edfaaa86e257bbf7f537b
Catalina Carreto <iamkcoder@gmail.com>
2022-01-24 20:27:25 -0600

    test: Create tes fire actions inside the modal

--- src/tests/components/calendar/CalendarModal.test.js
+++ src/tests/components/calendar/CalendarModal.test.js
@@ -10,58 +10,83 @@ import moment from 'moment';
 import '@testing-library/jest-dom';
 
 import { CalendarModal } from '../../../components/calendar/CalendarModal';
-
-/*jest.mock('../../../actions/events', () => ({
-    eventSetActive: jest.fn(),
-    eventStartLoading: jest.fn()
-}));*/
+import { eventStartUpdated, eventClearActiveEvent } from '../../../actions/events';
 
 
-const middleware = [thunk];
-const mockStore = configureStore(middleware);
+jest.mock('../../../actions/events', () => ({
+    eventStartUpdated: jest.fn(),
+    eventClearActiveEvent: jest.fn(),
+}))
 
-const now = moment().minutes(0).seconds(0).add(1, 'hours');//11:04:00
+const middlewares = [ thunk ];
+const mockStore = configureStore( middlewares );
+
+const now = moment().minutes(0).seconds(0).add(1,'hours'); // 3:00:00
 const nowPlusOne = now.clone().add(1, 'hours');
-
-const initEvent = {
-    title: '',
-    notes: '',
-    start: now.toDate(),
-    and: nowPlusOne.toDate()
-}
 
 const initState = {
     calendar: {
         events: [],
         activeEvent: {
-            title: 'Hola mundo',
+            title: 'Hola Mundo',
             notes: 'Algunas notas',
             start: now.toDate(),
-            and: nowPlusOne.toDate()
+            end: nowPlusOne.toDate()
         }
     },
     auth: {
         uid: '123',
-        name: 'Katy'
+        name: 'katy'
     },
     ui: {
         modalOpen: true
     }
 };
 
-const store = mockStore( initState);
+const store = mockStore( initState );
 store.dispatch = jest.fn();
 
-const wrapper = mount(
-    <Provider store = {store} >
-        <CalendarModal /> 
-    </Provider>
-)
 
-describe(`Pruebas en <CalendarModal />`, () => {
+const wrapper = mount(
+    <Provider store={ store } >
+        <CalendarModal />
+    </Provider>
+);
+
+
+describe('Pruebas en <CalendarModal />', () => {
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+    })
+
     
-    test(`debe de mostrar el modal`, () => {
+    test('debe de mostrar el modal', () => {
+        
         expect( wrapper.find('Modal').prop('isOpen') ).toBe(true);
-    
+
     });
+
+    test('debe de llamar la acción de actualizar y cerrar modal', () => {
+        
+        wrapper.find('form').simulate('submit', {
+            preventDefault(){}
+        });
+
+        expect( eventStartUpdated ).toHaveBeenCalledWith( initState.calendar.activeEvent );
+        expect( eventClearActiveEvent ).toHaveBeenCalled();
+
+    });
+
+    test('debe de mostrar error si falta el titulo', () => {
+
+        wrapper.find('form').simulate('submit', {
+            preventDefault(){}
+        });
+
+        expect( wrapper.find('input[name="title"]').hasClass('is-invalid') ).toBe(true);
+        
+    });
+
+    
 });
